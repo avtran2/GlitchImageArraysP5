5b0e9e5212d5aadf0c6341bb291b4c053997fa7f
Alvin Tran <47397365+avtran2@users.noreply.github.com>
2020-02-18 16:08:36 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 /*
-	Glitch Image Arrays
-	by: Alvin Tran
+    Glitch Image Arrays
+    by: Alvin Tran
 
-	Use images and other shape-drawing techniques with timers using millis() to create erratic behavior.
+    Use images and other shape-drawing techniques with timers using millis() to create erratic behavior.
 
 */
 
@@ -21,48 +21,52 @@ var stateList;
 
 // preload() will execture before setup()
 function preload(){
-  imageList[0]=loadImage('image1.jpg'); 			
+  imageList[0]=loadImage('image1.jpg');             
   imageList[1]=loadImage('image2.jpg');
   imageList[2]=loadImage('image3.jpg');
   imageList[3]=loadImage('image4.jpg');
   imageList[4]=loadImage('image5.jpg');
-  stringList=["Hi there!"];
+  stringList=["Hi there!", "Hello!", "How are you doing?"];
 }
 
 function setup(){
-	print("Glitch Image Array");
+    print("Glitch Image Array");
 
-	imageMode(CENTER);
+    imageMode(CENTER);
 
-	chooseNewImage();
+    chooseNewImage();
   
-	createCanvas(1024, 800);
+    createCanvas(1024, 800);
 
-	startMillis=millis();
+    startMillis=millis();
 }
 
 function draw(){
-	if(state=="Ellipse")
-		drawOne();
-	else if(state=="mouseFollowYellow")
-		drawTwo();
-	else if(state=="displayText")
-		drawThree();
-	else if(state=="mouseFollowGreen")
-		drawFour();
-	else if(state=="mouseFollowRed")
-		drawFive();
-	else if(state="normalPic")
-		drawZero();
+    if(state=="redEllipse")
+        drawRedEllipse();    
+    else if(state=="yellowEllipse")
+        drawYellowEllipse();
+    else if(state=="greenEllipse")
+        drawGreenEllipse();
+    else if(state=="displayText")
+        drawText();
+    else if(state=="mouseFollowRed")
+        drawRedEllipse();
+    else if(state=="mouseFollowYellow")
+        drawYellowFollow(); 
+    else if(state=="mouseFollowGreen")
+        drawGreenFollow();
+    else if(state="normalPic")
+        drawNormal();
 }
 
-function drawZero(){//Normal display
-	background(0);//Black
+function drawNormal(){//Normal display
+    background(0);//Black
     image(img, width/2, height/2);
 }
 
-function drawOne(){//Display a red ellipse behind the picture
-	background(0);
+function drawRedEllipse(){//Display a red ellipse behind the picture
+    background(0);
 
     //Displays the image at center point
 
@@ -76,10 +80,66 @@ function drawOne(){//Display a red ellipse behind the picture
     image(img, width/2, height/2);
 }
 
-function drawTwo(){//Display a yellow ellipse that follows the mouse
-	background(0);//Black
+function drawYellowEllipse(){//Display a yellow ellipse behind the picture
+    background(0);
 
     //Displays the image at center point
+
+    fill(255, 255, 0);//Yellow
+
+    var pictureDiameter=width/1.2;
+    ellipse((width/2), (height/2), pictureDiameter, pictureDiameter);
+
+
+    //draw the image
+    image(img, width/2, height/2);
+}
+
+function drawGreenEllipse(){//Display a green ellipse behind the picture
+    background(0);
+
+    //Displays the image at center point
+
+    fill(0, 128, 0);//Green
+
+    var pictureDiameter=width/1.2;
+    ellipse((width/2), (height/2), pictureDiameter, pictureDiameter);
+
+
+    //draw the image
+    image(img, width/2, height/2);
+}
+
+function drawText(){//Display text
+    background(255, 0, 0);    
+
+    //draw the image
+    image(img, width/2, height/2);
+
+    fill(255, 255, 0);
+    text(displayString, 100, 100);
+}
+
+function drawRedFollow(){//Display a red ellipse that follows the mouse
+    background(0);//Black
+
+    //draw the image
+    image(img, width/2, height/2);
+
+    posX=mouseX;
+    posY=mouseY;
+
+    posX+=(mouseX-posX)/10;
+    posY+=(mouseY-posY)/10;
+
+    fill(255, 0, 0);//Red
+
+    ellipse(posX, posY, 100, 100);
+}
+
+function drawYellowFollow(){//Display a yellow ellipse that follows the mouse
+    background(0);//Black
+
     //draw the image
     image(img, width/2, height/2);
 
@@ -91,25 +151,12 @@ function drawTwo(){//Display a yellow ellipse that follows the mouse
 
     fill(255, 255, 0);//Yellow
 
-    ellipse(posX, posY, 100, 100);
-    
+    ellipse(posX, posY, 100, 100);    
 }
 
-function drawThree(){//Display text
-	background(255, 0, 0);    
+function drawGreenFollow(){//Display a green ellipse that follows the mouse
+    background(0);//Black
 
-    //draw the image
-    image(img, width/2, height/2);
-
-    chooseNewItem();
-    fill(255, 255, 0);
-    text(displayString, 100, 100);
-}
-
-function drawFour(){//Display a green ellipse that follows the mouse
-	background(0);//Black
-
-    //Displays the image at center point
     //draw the image
     image(img, width/2, height/2);
 
@@ -124,50 +171,34 @@ function drawFour(){//Display a green ellipse that follows the mouse
     ellipse(posX, posY, 100, 100);
 }
 
-function drawFive(){//Display a red ellipse that follows the mouse
-	background(0);//Black
-
-    //Displays the image at center point
-    //draw the image
-    image(img, width/2, height/2);
-
-    posX=mouseX;
-    posY=mouseY;
-
-    posX+=(mouseX-posX)/10;
-    posY+=(mouseY-posY)/10;
-
-    fill(255, 0, 0);//Red
-
-    ellipse(posX, posY, 100, 100);
-}
 
 //chooses a new items from the array, select a random
 //index 0 to length of array-1
 function chooseNewImage(){
-	img=random(imageList);
-	print(img);
+    img=random(imageList);
+    print(img);
 }
 
 // chooses a new items from the array, select a random
 // index 0 to length of array-1
-function chooseNewItem() {
+function chooseNewItem(){
   displayString=random(stringList);
   print(displayString);
 }
 
-function mousePressed() {
+function mousePressed(){//Checks to see if the mouse is pressed
   bRegular=!bRegular;
 
   setState();
 }
 
-function setState(){
-	stateList=["Ellipse", "mouseFollowYellow", "displayText", "mouseFollowGreen", "mouseFollowRed", "normalPic"];
+function setState(){//Sets the state from an array of strings
+    stateList=["redEllipse", "yellowEllipse", "greenEllipse", "displayText", "mouseFollowRed", "mouseFollowYellow", "mouseFollowGreen", "normalPic"];
 
     state=random(stateList);    
 
-    chooseNewImage();
+    chooseNewImage();    
+    chooseNewItem();
 
-	return state;
+    return state;
  }
